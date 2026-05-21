2422395529586220774b52f5b3bcf007e13bb7d8
BeingShashwat <shashwatm878@gmail.com>
2026-05-21 14:10:00 +0530

    feat: add remove tracking action with delete API calls

--- src/pages/CaseDetailPage.tsx
+++ src/pages/CaseDetailPage.tsx
@@ -108,5 +108,3 @@ export const CaseDetailPage: React.FC = () => {
     </div>
   )
 }
-
-// TODO: support case removal action
--- src/pages/CasesPage.tsx
+++ src/pages/CasesPage.tsx
@@ -170,5 +170,3 @@ export const CasesPage: React.FC = () => {
     </>
   )
 }
-
-// TODO: implement archive and delete cases handlers
